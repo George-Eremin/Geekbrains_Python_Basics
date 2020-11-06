bee581c56de0b62f659f2cc8ec1ceaf06dbda4d5
G.Eremin <george.eremin@gmail.com>
2020-11-06 23:53:21 +0300

    Первый пункт ДЗ, про переменные

---
+++ Lesson_1/L1_01_variables.py
@@ -0,0 +1,7 @@
+var_1: int = 1
+var_2 = 2
+
+name = input("Введите свое имя: ")
+favoriteNumber = int( input("Введите ваше любимое число, " + name + ": ") )
+
+print(f"Здравствуйте, {name}, ваше любимое число {favoriteNumber}")
