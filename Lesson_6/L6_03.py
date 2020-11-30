ab3578aada6474f40f3fa2d29d7216e35082eafc
G.Eremin <george.eremin@gmail.com>
2020-12-01 02:39:57 +0300

    Урок 6. Задания 3-5

--- Lesson_6/L6_03.py
+++ Lesson_6/L6_03.py
@@ -11,21 +11,30 @@
 """
 
 
-class Worker
-  name: str
-  surname: str
-  position: str
-  _income: {"wage": float, "bonus": float}
+class Worker:
+    name: str
+    surname: str
+    position: str
+    _income: {}
   
-  def __init(self, name:str, surname: str):
-    self.name = name
-    self.surname = surname
-    self._income = {wage: 100, bonus: 20} 
-
-class Position(Worker)
-  def get_full_name():
-    return (f'{self.name} {self.surname})
-  
-  
+    def __init__(self, name: str, surname: str, position: str, wage: float, bonus: float):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
 
 
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+
+worker_1 = Position('Иван', 'Козловский', 'певец', 100, 20)
+
+print(f'Создан объект Position с атрибутами: \n'
+      f'name: {worker_1.name}, surname: {worker_1.surname}, position: {worker_1.position}\n')
+print(f'Результат работы метода get_full_name: {worker_1.get_full_name()}')
+print(f'Результат работы метода get_total_income: {worker_1.get_total_income()}')
--- Lesson_6/L6_04.py
+++ Lesson_6/L6_04.py
@@ -9,3 +9,106 @@
 Создайте экземпляры классов, передайте значения атрибутов.
 Выполните доступ к атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
 """
+
+
+class Car:
+    speed: float
+    color: str
+    name: str
+    is_police: bool
+
+    def __init__(self, color: str, name: str, is_police: bool):
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self, speed: float):
+        if speed > 0:
+            self.speed = speed
+            print(f'Машина поехала со скоростью {speed} км/ч')
+        else:
+            print('Скорость должна быть больше нуля')
+
+    def stop(self):
+        self.speed = 0
+        print('Машина остановилась')
+
+    def turn(self, direction: str):
+        print(f'Машина повернула {direction}')
+
+    def show_speed(self):
+        if self.speed > 0:
+            print(f'Машина движется со скоростью: {self.speed} км/ч')
+        elif self.speed == 0:
+            print('Машина стоит на месте')
+        else:
+            print('Ошибка со скоростью машины')
+
+
+class TownCar(Car):
+    def __init__(self, color: str, name: str):
+        super().__init__(color=color, name=name, is_police=False)
+
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'Машина движется со скоростью: {self.speed} км/ч\n'
+                  f'    !!! Внимание !!! Превышение скорости! Максимальная разрешенная скорость - 60 км/ч')
+        elif self.speed > 0:
+            print(f'Машина движется со скоростью: {self.speed} км/ч')
+        elif self.speed == 0:
+            print('Машина стоит на месте')
+        else:
+            print('Ошибка со скоростью машины')
+
+
+class WorkCar(Car):
+    def __init__(self, color: str, name: str):
+        super().__init__(color=color, name=name, is_police=False)
+
+    def show_speed(self):
+        if self.speed > 40:
+            print(f'Машина движется со скоростью: {self.speed} км/ч\n'
+                  f'   !!! Внимание!!! Превышение скорости! Максимальная разрешенная скорость - 40 км/ч')
+        elif self.speed > 0:
+            print(f'Машина движется со скоростью: {self.speed} км/ч')
+        elif self.speed == 0:
+            print('Машина стоит на месте')
+        else:
+            print('Ошибка со скоростью машины')
+
+
+class SportCar(Car):
+    def __init__(self, color: str, name: str):
+        super().__init__(color=color, name=name, is_police=False)
+
+
+class PoliceCar(Car):
+    def __init__(self, color: str, name: str):
+        super().__init__(color=color, name=name, is_police=True)
+
+
+# Создаем массив машин разных типов
+cars = [
+    SportCar(color="красная", name="спортивная машина"),
+    TownCar(color="белая", name="городская машина"),
+    WorkCar(color="черная", name="рабочая машина"),
+    PoliceCar(color="синяя", name="полицейская машина")
+]
+
+# Для машины каждого типа
+for car in cars:
+    # Выводим атрибуты
+    print('\n--------------------------------')
+    print(f'Тип: {type(car)}')
+    print(f'Цвет: {car.color}')
+    print(f'Название: {car.name}')
+    print(f'Является ли полицейской: {car.is_police}')
+
+    # Вызываем методы
+    car.go(100)
+    car.turn("направо")
+    car.show_speed()
+    car.go(20)
+    car.show_speed()
+    car.stop()
+    car.show_speed()
